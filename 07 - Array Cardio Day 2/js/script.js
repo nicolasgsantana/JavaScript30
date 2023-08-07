d51c9a56b938fa6d4f76c725c94d33b75c7608b8
nicolasgsantana <nicolas07san@gmail.com>
2023-08-07 16:40:48 -0300

    Finish exercises

--- 07 - Array Cardio Day 2/js/script.js
+++ 07 - Array Cardio Day 2/js/script.js
@@ -14,15 +14,24 @@ const comments = [
     { text: 'Ramen is my fav food ever', id: 123523 },
     { text: 'Nice Nice Nice!', id: 542328 }
 ];
-  
+
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const someoneIsAdult = people.some(person => new Date().getFullYear() - person.year >= 19);
+console.log({ someoneIsAdult });
 // Array.prototype.every() // is everyone 19 or older?
-  
+const everyoneIsAdult = people.every(person => new Date().getFullYear() - person.year >= 19);
+console.log({ everyoneIsAdult });
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-  
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment)
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+comments.splice(index, 1);
